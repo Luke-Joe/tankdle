443ac5a5ac53809be7781ee04d62382fa790d936
Luke <luke.lukejoe@gmail.com>
2024-07-25 00:37:44 -0700

    FL and CL vehicles are now filtered out of the dataset

--- server/models/tankModel.js
+++ server/models/tankModel.js
@@ -2,7 +2,6 @@ import axios from 'axios';
 import { readData, storeData, appendData } from '../utils/fileUtils.js';
 import {API_URL, APP_ID, TANK_DATA_FILE, TANK_SOL_FILE, PREV_SOL_FILE} from '../config/constants.js';
 
-let tankData = [];
 export async function fetchTankData() {
     try { 
         const response = await axios.get(API_URL, {
@@ -11,7 +10,8 @@ export async function fetchTankData() {
                 fields: "name,tank_id,tier,nation,type,is_premium,default_profile.gun.caliber",
             },
         });
-        tankData = Object.values(response.data.data);
+        let tankData = Object.values(response.data.data);
+        tankData = updateData(tankData);
         storeData(TANK_DATA_FILE, tankData);
         return tankData;
     } catch (error) {
@@ -20,20 +20,59 @@ export async function fetchTankData() {
     }
 }
 
-function loadTankData() {
-    const data = readData(TANK_DATA_FILE);
-    if (data) {
-        tankData = data;
+export async function getTankList() {
+    if (!readData(TANK_DATA_FILE)) {
+        await fetchTankData();
     }
+
+    return readData(TANK_DATA_FILE);
 }
 
-function getRandomTank(excludeTank) {
+function updateData(tankData) {
+    tankData = filterData(tankData);
+
+    return tankData;
+}
+
+function updateGunInformation(tankData) {
+    
+}
+
+function filterData(tankData) {
+    return tankData.filter(tank => {
+        return !tank.name.endsWith(" FL") && !tank.name.endsWith(" CL");
+    })
+}
+
+function getRandomTank(tankData, excludeTank) {
     let randomTank;
     do {
         randomTank = tankData[Math.floor(Math.random() * tankData.length)];
     } while (excludeTank && randomTank.tank_id === excludeTank.tank_id);
 
     return randomTank;
+}
+
+export async function updateSolutionTank() {
+    let tankData = await getTankList();
+
+    const prevData = readData(TANK_SOL_FILE);
+    const prevSolTank = prevData?.solutionTank
+    savePrevSol();
+    const newSolTank = getRandomTank(tankData, prevSolTank);
+    const newDayId = (prevData.dayId || 0) + 1;
+
+    storeData(TANK_SOL_FILE, {solutionTank : newSolTank, dayId : newDayId});
+    return { solutionTank : newSolTank, dayId : newDayId };
+}
+
+export async function getSolutionTank() {
+    if (!readData(TANK_SOL_FILE)) {
+        await updateSolutionTank(await getTankList());
+    }
+
+    const data = readData(TANK_SOL_FILE);
+    return { solutionTank: data.solutionTank, dayId: data.dayId};
 }
 
 async function savePrevSol() {
@@ -45,42 +84,6 @@ async function savePrevSol() {
     console.log("Saved previous solution tank:", prevData.solutionTank.name);
 }
 
-export async function updateSolutionTank() {
-    if (tankData.length === 0) {
-        loadTankData();
-    }
-
-    if (tankData.length === 0) {
-        tankData = await fetchTankData();
-    }
-
-    const prevData = readData(TANK_SOL_FILE);
-    const prevSolTank = prevData?.solutionTank
-    savePrevSol();
-    const newSolTank = getRandomTank(prevSolTank);
-    const newDayId = (prevData.dayId || 0) + 1;
-
-    storeData(TANK_SOL_FILE, {solutionTank : newSolTank, dayId : newDayId});
-    return { solutionTank : newSolTank, dayId : newDayId };
-}
-
-export async function getSolutionTank() {
-    if (!readData(TANK_SOL_FILE)) {
-        await updateSolutionTank();
-    }
-
-    const data = readData(TANK_SOL_FILE);
-    return { solutionTank: data.solutionTank, dayId: data.dayId};
-}
-
-export async function getTankList() {
-    if (!readData(TANK_DATA_FILE)) {
-        await fetchTankData();
-    }
-
-    return readData(TANK_DATA_FILE);
-}
-
 export async function popPrevSolution() {
     const prevData = readData(PREV_SOL_FILE);
 
@@ -90,3 +93,6 @@ export async function popPrevSolution() {
 
     return null;
 }
+
+
+
