e6448dc7709dd2d927ee4fd56478030e9ff59f61
Luke <luke.lukejoe@gmail.com>
2024-07-24 18:54:55 -0700

    Added utils to allow for saving of past solutions

--- server/config/constants.js
+++ server/config/constants.js
@@ -7,3 +7,4 @@ export const API_URL = "https://api.wotblitz.com/wotb/encyclopedia/vehicles/";
 export const APP_ID = "c1eff31a5be5648b3b8ed22d23f2d94d";
 export const TANK_DATA_FILE = path.resolve(__dirname, '../data/tankData.json');
 export const TANK_SOL_FILE = path.resolve(__dirname, '../data/tankSol.json');
+export const PREV_SOL_FILE = path.resolve(__dirname, '../data/prevSols.json');
--- server/utils/fileUtils.js
+++ server/utils/fileUtils.js
@@ -1,13 +1,33 @@
 import fs from 'fs';
 
-export function readData(filePath) {
-    if (fs.existsSync(filePath)) {
-        return JSON.parse(fs.readFileSync(filePath, 'utf8'));
-    } else {
+export function readData(filepath) {
+    try {
+        const data = fs.readFileSync(filepath, 'utf8');
+        return JSON.parse(data);
+    } catch (error) {
+        console.error("Error fetching data from ${filepath}:", error);
         return null;
-    }
+    } 
 }
 
 export function storeData(filepath, data) {
-    fs.writeFileSync(filepath, JSON.stringify(data, null, 2), 'utf-8');
+    try {
+        fs.writeFileSync(filepath, JSON.stringify(data, null, 2), 'utf-8');
+    } catch (error) {
+        console.error("Error storing data to ${filepath}:", error);
+    } 
+}
+
+export function appendData(filepath, data) {
+    try {
+        let existingData = [];
+        if (fs.existsSync(filepath)) {
+            existingData = readData(filepath);
+        }
+
+        existingData.push(data);
+        fs.writeFileSync(filepath, JSON.stringify(existingData, null, 2), 'utf-8');
+    } catch (error) {
+        console.error("Error appending data to ${filepath}:", error);
+    }
 }
