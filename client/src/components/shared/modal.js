10815146d608743091c5145487fdac5a88fef01a
Luke <luke.lukejoe@gmail.com>
2024-08-08 11:10:25 -0700

    Fixed modal behaviour

--- client/src/components/shared/modal.js
+++ client/src/components/shared/modal.js
@@ -11,14 +11,14 @@ const Modal = ({ show, onClose, children }) => {
 
     return (
         <div className="fixed inset-0 bg-gray-900 bg-opacity-80 flex justify-center items-center z-50 ease-in-out animate-fade-in" onClick={handleBackgroundClick}>
-            <div className="bg-wblack rounded shadow-lg p-4 relative mx-auto border-4 border-double border-wgray max-w-3xl max-h-screen top-10">
+            <div className="bg-wblack rounded shadow-lg p-4 relative mx-auto border-4 border-double border-wgray max-w-3xl max-h-[80vh] overflow-auto">
                 <button
                     onClick={onClose}
                     className="absolute top-2 right-3 text-red-400 hover:text-red-500 focus:outline-none text-2xl"
                 >
                     &times;
                 </button>
-                <div className="max-h-screen overflow-auto ">
+                <div className="h-full overflow-auto ">
                     {children}
                 </div>
             </div>
