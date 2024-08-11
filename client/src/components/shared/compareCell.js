c7ffc2ed1bd7c462ad259e052094c58af5365374
Luke <luke.lukejoe@gmail.com>
2024-08-11 14:28:35 -0700

    Increased contrast between arrow and background for mobile devices

--- client/src/components/shared/compareCell.js
+++ client/src/components/shared/compareCell.js
@@ -4,8 +4,8 @@ import substituteValues from '../../utils/mapValues.js';
 export default function CompareCell({ guessAttribute }) {
     return (
         <div className={`${guessAttribute.comparison} compareCell relative`}>
-            {guessAttribute.comparison === 'blue' && <img src="/arrow.svg" alt="Higher" className="absolute transform rotate-90 z-0 opacity-40 w-3/5 min-w-14" />}
-            {guessAttribute.comparison === 'yellow' && <img src="/arrow.svg" alt="Lower" className="absolute transform -rotate-90 z-0 opacity-40 w-3/5 min-w-14" />}
+            {guessAttribute.comparison === 'blue' && <img src="/arrow.svg" alt="Higher" className="absolute transform rotate-90 z-0 opacity-70 w-3/5 min-w-14" />}
+            {guessAttribute.comparison === 'yellow' && <img src="/arrow.svg" alt="Lower" className="absolute transform -rotate-90 z-0 opacity-70 w-3/5 min-w-14" />}
             <div className="z-10">{substituteValues(guessAttribute)}</div>
         </div>
     )
